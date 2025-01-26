3508c4b91a23a0f64adcca785ebaf78e53923ee1
Vilhelms <vilhelmsbavins123@gmail.com>
2025-01-26 22:26:26 +0200

    Added $HOME for universal support

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,3 +1,3 @@
 Invoke-Expression (& { (zoxide init powershell | Out-String) })
-oh-my-posh init pwsh --config 'C:\Users\Saimnieks\AppData\Local\Programs\oh-my-posh\themes\onehalf.minimal.omp.json' | Invoke-Expression
+oh-my-posh init pwsh --config "$HOME\AppData\Local\Programs\oh-my-posh\themes\onehalf.minimal.omp.json" | Invoke-Expression
 Set-PSReadLineOption -PredictionViewStyle ListView
